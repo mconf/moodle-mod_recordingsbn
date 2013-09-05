2468082f32ccd3e5c427ba27b4e16eb5516443b6
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2013-09-05 12:28:08 -0300

    Add language file for pt-br for Mconf, contributed by @lucianoes

---
+++ lang/pt_br/recordingsbn.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Language File
+ *
+ * Authors:
+ *    Jesus Federico  (jesus [at] blindsidenetworks [dt] com)
+ *    
+ *
+ * @package   mod_bigbluebutton
+ * @copyright 2011-2012 Blindside Networks Inc.
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v2 or later
+ * Traduzido para o portugues por Luciano Silva lucianoes@ufrgs.br para uso com o Mconf
+ * 
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'Mconf Gravações Webconferências ';
+$string['modulenameplural'] = 'Mconf Gravações Webconferências ';
+$string['modulename_help'] = 'Use o recurso "Mconf Gravações Webconferências" para disponibilizar as gravações relacionadas ao curso. 
+							IMPORTANTE: As gravações estarão disponíveis após 20 minutos do encerramento de cada sessão.';
+$string['modulename_link'] = 'Mconf Gravações/visualizar';
+$string['recordingsbnname'] = 'Nome das gravações';
+$string['recordingsbnname_help'] = 'Mconf Gravações Webconferências  disponibiliza a lista das gravações efetuadas pelo servidor Mconf Webconferências.';
+$string['recordingsbn'] = 'Mconf Gravações Webconferências';
+$string['pluginadministration'] = 'Administração Mconf Gravações Webconferências';
+$string['pluginname'] = ' Mconf Gravações Webconferências';
+$string['recordingsbn:addinstance'] = 'Adicione um novo recurso com player para gravações';
+$string['recordingsbn:view'] = 'Visualizar gravações';
+$string['view_delete_confirmation'] = 'Tem certeza que deseja deletar esta gravação?';
+$string['view_dependency_error'] = 'Você precisa ter o módulo de atividades "Mconf Webconferência" instalado';
+$string['view_head_actionbar'] = 'Ferramentas';
+$string['view_head_activity'] = 'Atividade';
+$string['view_head_course'] = 'Curso';
+$string['view_head_date'] = 'Data';
+$string['view_head_description'] = 'Descrição';
+$string['view_head_length'] = 'Extensão';
+$string['view_head_duration'] = 'Duração';
+$string['view_duration_min'] = 'min';
+$string['view_head_recording'] = 'Gravação';
+$string['view_noguests'] = 'O módulo Mconf Gravações não é aberto para convidados.';
