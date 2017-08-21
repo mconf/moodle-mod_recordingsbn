1f8aedd2a59ba8dc8e7e056ae0fcfd856752d61e
Felipe Cecagno <fcecagno@gmail.com>
2017-08-21 09:21:06 -0300

    update pt_br strings

--- lang/pt_br/recordingsbn.php
+++ lang/pt_br/recordingsbn.php
@@ -5,7 +5,7 @@
  * Authors:
  *    Jesus Federico  (jesus [at] blindsidenetworks [dt] com)
  *    Luciano Silva  (lucianoes [at] ufrgs [dt] br)
- *    Felipe Cecagno  (felipe [at] mconf [dt] org)
+ *    Felipe Cecagno  (felipe [at] mconf [dt] com)
  *    
  *
  * @package   mod_bigbluebutton
@@ -19,14 +19,29 @@ $string['modulenameplural'] = 'Gravações Mconf';
 $string['modulename_help'] = 'Use o recurso "Gravações Mconf" para disponibilizar as gravações relacionadas ao curso. 
 
 IMPORTANTE: As gravações estarão disponíveis após 20 minutos do encerramento de cada sessão.';
-$string['modulename_link'] = 'RecordingsBN/visualizar';
+$string['modulename_link'] = 'Gravações Mconf/view';
 $string['recordingsbnname'] = 'Nome das gravações';
 $string['recordingsbnname_help'] = 'Gravações Mconf disponibiliza a lista das gravações efetuadas pelo plugin Webconferência Mconf.';
 $string['recordingsbn'] = 'Gravações Mconf';
 $string['pluginadministration'] = 'Administração das Gravações Mconf';
-$string['pluginname'] = ' Gravações Mconf';
-$string['recordingsbn:addinstance'] = 'Adicione um novo recurso com player para gravações';
+$string['pluginname'] = 'Gravações Mconf';
+$string['recordingsbn:addinstance'] = 'Adicionar um novo recurso para reproduzir gravações';
 $string['recordingsbn:view'] = 'Visualizar gravações';
+
+$string['mod_form_field_ui_html'] = 'Mostrar a tabela em html plano';
+$string['mod_form_field_include_deleted_activities'] = 'Incluir gravações de atividades removidas';
+
+$string['config_general'] = 'Configurações gerais';
+$string['config_general_description'] = 'Essas configurações são usadas <b>sempre</b>';
+$string['config_feature_ui_html_default'] = 'Interface como html é habilitado por padrão';
+$string['config_feature_ui_html_default_description'] = 'Se habilitado a tabela de gravações é mostrada como HTML plano por padrão.';
+$string['config_feature_ui_html_editable'] = 'Opção de interface como html pode ser modificada';
+$string['config_feature_ui_html_editable_description'] = 'Por padrão a opção de interface como html pode ser editada quando o Gravações Mconf é adicionado ou atualizado.';
+$string['config_feature_include_deleted_activities_default'] = 'Incluir gravações de atividades removidas habilitado por padrão';
+$string['config_feature_include_deleted_activities_default_description'] = 'Se habilitado a tabela de gravações irá incluir as gravações realizadas em atividades que já foram removidas.';
+$string['config_feature_include_deleted_activities_editable'] = 'Opção de incluir gravações de atividades removidas pode ser modificada';
+$string['config_feature_include_deleted_activities_editable_description'] = 'Por padrão a opção de incluir gravações de atividades removidas pode ser modificada quando o Gravações Mconf é adicionado ou atualizado.';
+
 $string['view_noguests'] = 'O módulo Gravações Mconf não é aberto para convidados.';
 $string['view_delete_confirmation'] = 'Tem certeza que deseja excluir esta gravação?';
 $string['view_dependency_error'] = 'Você precisa ter o módulo de atividades "Webconferência Mconf" instalado';
@@ -35,7 +50,13 @@ $string['view_head_activity'] = 'Atividade';
 $string['view_head_course'] = 'Curso';
 $string['view_head_date'] = 'Data';
 $string['view_head_description'] = 'Descrição';
-$string['view_head_length'] = 'Extensão';
+$string['view_head_length'] = 'Tamanho';
 $string['view_head_duration'] = 'Duração';
 $string['view_head_recording'] = 'Gravação';
 $string['view_duration_min'] = 'min';
+$string['view_no_recordings'] = 'Não há nenhuma gravação para exibir';
+
+$string['event_resource_page_viewed'] = 'Página Gravações Mconf visualizada';
+$string['event_recording_published'] = 'Gravação publicada';
+$string['event_recording_unpublished'] = 'Gravação despublicada';
+$string['event_recording_deleted'] = 'Gravação apagada';
